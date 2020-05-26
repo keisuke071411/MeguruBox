ec7e03f2032c8a87fc0722598023fd507a981e70
keisuke071411 <i.love.football0714@gmail.com>
2020-05-26 20:20:09 +0900

    jsの機能を実装しました

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const meguru = document.getElementById('meguru');
 meguru.insertAdjacentHTML('beforeend', '\
+<div id="bigBox">\
 <div id="scene">\
 <div class="boxBase">\
     <div class="top"></div>\
@@ -19,7 +20,23 @@ meguru.insertAdjacentHTML('beforeend', '\
     <div class="left"></div>\
     <div class="right"></div>\
 </div>\
+</div>\
 </div>');
 
 const mouse = document.getElementById('scene');
 const box = document.querySelector('div');
+
+let count = 0;
+
+function bigClone() {
+    setTimeout(bigClone, 3000);
+    if (count <= 2) {
+        let bigBox = document.getElementById('bigBox');
+        let clone = bigBox.firstElementChild.cloneNode(true);
+        bigBox.appendChild(clone); 
+        count ++;
+
+    }
+};
+
+bigClone();
